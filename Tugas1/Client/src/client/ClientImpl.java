833de2798ad6a6b10b653066ed4b6767da72fc79
Fendy <fendy.fendy95@gmail.com>
2015-10-02 23:15:13 +0700

    Client-Server
    Client server implemented

---
+++ Tugas1/Client/src/client/ClientImpl.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author fendy
+ */
+public class ClientImpl {
+    public void start(){
+        try {
+            
+            Scanner scanner = new Scanner(System.in);
+            String test = scanner.nextLine();
+            
+            while (!test.contentEquals("exit")) {
+                Socket socket = new Socket("localhost", 8888);
+                InputStream is = socket.getInputStream();
+                OutputStream os = socket.getOutputStream();
+                os.write(test.getBytes());
+                os.flush();
+                while (true) {
+                    byte[] buf = new byte[10];
+                    int len = is.read(buf);
+                    if(len == -1){
+                        break;
+                    }
+                    System.out.print(new String(buf));
+                }
+                byte[] buf = new byte[1024];
+                is.read(buf);
+                System.out.println(new String(buf));
+                test = scanner.nextLine();
+                os.close();
+                is.close();
+            }
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ Tugas1/Server/src/server/ServerImpl.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author fendy
+ */
+public class ServerImpl {
+    public void start(){
+        try {
+            
+            int port = 8888;
+            ServerSocket serversock = new ServerSocket(port);
+            System.out.println("Server started listening on " + port);
+            while (true) {
+                Socket socket = serversock.accept();
+                System.out.println("Client connected ...");
+                InputStream is = socket.getInputStream();
+                OutputStream os = socket.getOutputStream();
+
+                byte[] buf = new byte[1024]; 
+                int len = is.read(buf);
+                System.out.println("Client said: " + new String(buf));
+
+                os.write(buf);
+                os.flush();
+                os.close();
+                is.close();
+                socket.close();
+            }
+            serversock.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ServerImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
